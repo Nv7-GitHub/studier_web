fcf04740358d662f94c4c63942d94e2f652f555a
Nv7-Github <junk4Nv7@gmail.com>
2024-02-09 23:11:48 -0800

    Single and multiple

--- src/lib/Quiz.svelte
+++ src/lib/Quiz.svelte
@@ -3,7 +3,9 @@
     import { onMount, tick } from "svelte";
 
     export let question: RecordModel;
+    let oldid = "";
     export let next: (correct: boolean) => Promise<void>;
+
     let inp: HTMLInputElement;
     let inpval: string;
 
@@ -12,12 +14,17 @@
     let wrong = false;
     let right = false;
     let wrongval: string;
+    let answered: string[] = [];
     $: {
-        parts = question.question.split(/(`.+?`)/g);
-        wrong = false;
-        right = false;
-        if (inp) {
-            inp.value = "";
+        if (oldid != question.id) {
+            oldid = question.id;
+            parts = question.question.split(/(`.+?`)/g);
+            wrong = false;
+            right = false;
+            answered = [];
+            if (inp) {
+                inp.value = "";
+            }
         }
     }
 
@@ -47,8 +54,10 @@
                 wrong = false;
                 right = true;
                 next(true);
+                return;
             } else {
                 next(false);
+                return;
             }
         }
 
@@ -57,10 +66,47 @@
                 console.log(inpval, question.answer.value);
                 right = true;
                 next(true);
+                return;
             } else {
                 wrong = true;
                 wrongval = inpval ?? "";
                 inp.value = "";
+                return;
+            }
+        }
+
+        if (question.kind == "multiple") {
+            let correct = false;
+            for (let ans of question.answer.values) {
+                if (compare(inpval, ans)) {
+                    // Check if already answered
+                    let already = false;
+                    for (let a of answered) {
+                        if (compare(a, ans)) {
+                            already = true;
+                            break;
+                        }
+                    }
+                    if (!already) {
+                        correct = true;
+                        break;
+                    }
+                }
+            }
+            if (correct) {
+                answered = [...answered, inpval];
+                inp.value = "";
+
+                if (answered.length == question.answer.values.length) {
+                    right = true;
+                    next(true);
+                    return;
+                }
+
+                return;
+            } else {
+                wrong = true;
+                wrongval = inpval ?? "";
                 return;
             }
         }
@@ -94,13 +140,32 @@
         {/if}
     {/if}
 
+    {#if question.kind == "multiple" && !wrong}
+        <h3 class="mt-3">Your Answers</h3>
+        <ul class="list-group">
+            {#each answered as a}
+                <li class="list-group-item">{a}</li>
+            {/each}
+        </ul>
+    {/if}
+
     {#if wrong}
         {#if question.kind == "single"}
-            That answer was wrong. <br />
-            <b>Correct Answer: </b>
-            {question.answer.value}<br /><br />
-            <b>Your Answer:</b>
+            <h3>Your Answer</h3>
             {wrongval}
+
+            <h3>Correct Answer</h3>
+            {question.answer.value}
+        {:else if question.kind == "multiple"}
+            <h3>Your Answer</h3>
+            {wrongval}
+
+            <h3>Correct Answers</h3>
+            <ul class="list-group">
+                {#each question.answer.values as a}
+                    <li class="list-group-item">{a}</li>
+                {/each}
+            </ul>
         {/if}
     {/if}
 </div>
