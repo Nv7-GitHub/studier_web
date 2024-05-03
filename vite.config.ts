193379275eb69cedc1f4876507a31d923689b419
Nv7-Github <junk4Nv7@gmail.com>
2024-05-02 21:01:42 -0700

    Fix the config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,26 @@ import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), SvelteKitPWA()]
+	plugins: [sveltekit(), SvelteKitPWA({
+		strategies: 'generateSW',
+		manifest: {
+			short_name: 'Studier',
+			name: 'Studier',
+			start_url: '/',
+			scope: '/',
+			display: 'standalone',
+			theme_color: "#212529",
+			background_color: "#212529",
+			icons: [
+				{
+					src: '/pwa-512x512.png',
+					sizes: '512x512',
+					type: 'image/png',
+				},
+			],
+		},
+		injectManifest: {
+			globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2,mp3}']
+		},
+	})]
 });
